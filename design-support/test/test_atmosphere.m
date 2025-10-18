b4e06b3cfc428fb3c4a9607fb6c2a0994864347c
FinnBreu <finn.breuer@tuhh.de>
2025-10-18 18:26:20 +0200

    better plots

--- design-support/test/test_atmosphere.m
+++ design-support/test/test_atmosphere.m
@@ -3,11 +3,11 @@ clear
 alt = 0:0.1:40; % altitude in km
 
 for i = 1:length(alt)
-    [p_i, t_i, rho_i, mach_i] = model_airdata(alt(i)*1000);
-    p(i) = p_i;
-    t(i) = t_i;
-    rho(i) = rho_i;
-    mach(i) = mach_i;
+    air_i = model_airdata(alt(i)*1000);
+    p(i) = air_i.pressure;
+    t(i) = air_i.temperature;
+    rho(i) = air_i.density;
+    mach(i) = air_i.mach;
 end
 
 %% Plot
@@ -15,26 +15,38 @@ set(groot, 'defaultAxesTickLabelInterpreter','latex')
 set(groot, 'defaultLegendInterpreter','latex')
 set(groot, 'DefaultTextInterpreter', 'latex')
 
-figure(1)
+f_p = figure(1);
 plot(p/1000, alt)
-yline([max(alt), 32, 20, 11],'--', {"Stratosphere2", "Stratosphere", "Tropopause", "Troposphere"},'LabelVerticalAlignment','bottom', 'Interpreter','latex')
+yline([max(alt), 32, 20, 11],'--', {"Stratosphere 2", "Stratosphere 1", "Tropopause", "Troposphere"},'LabelVerticalAlignment','bottom', 'Interpreter','latex')
 ylim([min(alt), max(alt)])
-ylabel("Altitude $$l$$ [km]")
-xlabel("Pressure $$p$$ [kPa]")
-
-figure(2)
+ylabel("Altitude $\ell, \: [\mathrm{km}]$")
+xlabel("Pressure $p, \: [\mathrm{kPa}]$")
+ax = gca;
+ax.XGrid = 'on';
+xticks(0:20:100);
+fontsize(14,"points")
+ 
+f_t = figure(2);
 plot(t, alt)
-yline([max(alt), 32, 20, 11],'--', {"Stratosphere2", "Stratosphere", "Tropopause", "Troposphere"},'LabelVerticalAlignment','bottom', 'Interpreter','latex')
+yline([max(alt), 32, 20, 11],'--', {"Stratosphere 2", "Stratosphere 1", "Tropopause", "Troposphere"},'LabelVerticalAlignment','bottom', 'Interpreter','latex')
 ylim([min(alt), max(alt)])
-ylabel("Altitude $$l$$  [km]")
-xlabel("Temperature $$T$$ [K]")
+ylabel("Altitude $\ell, \: [\mathrm{km}]$")
+xlabel("Temperature $T, \: [\mathrm{K}]$")
+ax = gca;
+ax.XGrid = 'on';
+xticks(200:20:300);
+fontsize(14,"points")
 
-figure(3)
+f_d = figure(3);
 plot(rho, alt)
-yline([max(alt), 32, 20, 11],'--', {"Stratosphere2", "Stratosphere", "Tropopause", "Troposphere"},'LabelVerticalAlignment','bottom', 'Interpreter','latex')
+yline([max(alt), 32, 20, 11],'--', {"Stratosphere 2", "Stratosphere 1", "Tropopause", "Troposphere"},'LabelVerticalAlignment','bottom', 'Interpreter','latex')
 ylim([min(alt), max(alt)])
-ylabel("Altitude $$l$$  [km]")
-xlabel("Density $$\rho$$, [kg/m$$^{3}$$]")
+ylabel("Altitude $\ell, \: [\mathrm{km}]$")
+xlabel("Density $\rho, \: [\mathrm{kg}/\mathrm{m}^3]$")
+ax = gca;
+ax.XGrid = 'on';
+xticks(0:0.3:1.5);
+fontsize(14,"points")
 
 % figure(4)
 % plot(mach, alt)
@@ -42,6 +54,12 @@ xlabel("Density $$\rho$$, [kg/m$$^{3}$$]")
 % ylim([min(alt), max(alt)])
 % ylabel("Altitude $$l$$  [km]")
 % xlabel("Speed of sound $$c$$ [m/s]")
+
+%%
+
+exportgraphics(f_p, 'plots/plot_atmosphere-pressure.pdf')
+exportgraphics(f_t, 'plots/plot_atmosphere-temperature.pdf')
+exportgraphics(f_d, 'plots/plot_atmosphere-density.pdf')
 
 set(groot, 'defaultAxesTickLabelInterpreter','remove')
 set(groot, 'defaultLegendInterpreter','remove')
