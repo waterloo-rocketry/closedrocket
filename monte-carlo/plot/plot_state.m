60130b1599d2e3e8b4cafdbbddf43aa6aafe56b4
FinnBreu <finn.breuer@tuhh.de>
2025-11-04 15:01:21 +0100

    Added improved run and analyze

--- monte-carlo/plot/plot_state.m
+++ monte-carlo/plot/plot_state.m
@@ -15,52 +15,58 @@ function [plots] = plot_state(dataset, varargin)
         name = "";
     end
     
-    names = append(["qw","qx","qy","qz"],name);
+    % names = append(["qw","qx","qy","qz"],name);
+    names = ["qw","qx","qy","qz"];
     for i = 1:4
         stairs(plots.q, dataset.Time, dataset.q(:,i), 'DisplayName', names(i));
         hold(plots.q, 'on')
     end
-    %legend(plots.q, 'show', 'FontSize', 7)
+    legend(plots.q, 'show')
     title(plots.q, "Quaternion",'FontWeight','Normal')
 
-    names = append(["wx","wy","wz"],name);
+    % names = append(["wx","wy","wz"],name);
+    names = ["wx","wy","wz"];
     for i = 1:3
         stairs(plots.w, dataset.Time, dataset.w(:,i), 'DisplayName', names(i))
         hold(plots.w, 'on')
     end
-    %legend(plots.w, 'show')
+    legend(plots.w, 'show')
     title(plots.w, "Rates [rad/s]",'FontWeight','Normal')
 
-    names = append(["vx","vy","vz"],name);
+    % names = append(["vx","vy","vz"],name);
+    names = ["vx","vy","vz"];
     for i = 1:3
         stairs(plots.v, dataset.Time, dataset.v(:,i), 'DisplayName', names(i))
         hold(plots.v, 'on')
     end
-    %legend(plots.v, 'show')
+    legend(plots.v, 'show')
     title(plots.v, "Velocity [m/s]",'FontWeight','Normal')
 
-    names = append("alt",name);
+    % names = append("alt",name);
+    names = "alt";
     for i = 1
         stairs(plots.alt, dataset.Time, dataset.alt(:,i) / 1000, 'DisplayName', names(i))
         hold(plots.alt, 'on')
     end
-    %legend(plots.alt, 'show')
+    legend(plots.alt, 'show')
     title(plots.alt, "Altitude [km]",'FontWeight','Normal')
 
     names = append("CL",name);
+    names = "CL";
     for i = 1
         stairs(plots.cl, dataset.Time, dataset.cl(:,i), 'DisplayName', names(i))
         hold(plots.cl, 'on')
     end
-    %legend(plots.cl, 'show')
+    legend(plots.cl, 'show')
     title(plots.cl, "Canard Coefficient",'FontWeight','Normal')
 
-    names = append("delta",name);
+    % names = append("delta",name);
+    names = "delta";
     for i = 1
         stairs(plots.delta, dataset.Time, rad2deg(dataset.delta(:,i)), 'DisplayName', names(i))
         hold(plots.delta, 'on')
     end
-    %legend(plots.delta, 'show')
+    legend(plots.delta, 'show')
     title(plots.delta, "Canard Angle [deg]",'FontWeight','Normal')
 
     if  nargin == 3 || nargin == 4
