a4d36ca50b50d3b6dfeeba05131f105b27217ec6
FinnBreu <finn.breuer@tuhh.de>
2025-11-06 23:28:10 +0100

    plotting

--- analysis/actuator/actuator_response_reef.m
+++ analysis/actuator/actuator_response_reef.m
@@ -49,9 +49,13 @@ hold on
 t = 0:sampling_time:0.2;
 y = lsim(sys_est, ones(length(t), 1), t);
 stairs(t, y, "k")
-% step(sys_sim, "k--")
-xlim([0,0.15]);
+% step(sys_sim, "k--")#
 hold off
+xlim([0,0.15]);
+grid on
+fontsize(12,"points")
+
+
 
 set(groot, 'defaultAxesTickLabelInterpreter','latex')
 set(groot, 'defaultLegendInterpreter','latex')
@@ -61,7 +65,7 @@ title("");
 xlabel("Time [s]")
 ylabel("Angle (normalized)")
 legend("Run 1", "Run 2", "Run 3", "Run 4", "Model", "Location","best")
-exportgraphics(fig, 'analysis/actuator/plot_actuator.pdf')
+exportgraphics(fig, 'plots/plot_actuator.pdf')
 
 set(groot, 'defaultAxesTickLabelInterpreter','remove')
 set(groot, 'defaultLegendInterpreter','remove')
