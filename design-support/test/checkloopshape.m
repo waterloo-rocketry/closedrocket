ac4eaae8f624d87b44541804a626a36ca13bde24
FinnBreu <finn.breuer@tuhh.de>
2025-11-12 00:35:33 +0100

    Controller plotting

--- design-support/test/checkloopshape.m
+++ design-support/test/checkloopshape.m
@@ -11,7 +11,12 @@
 % "Flight Control Law Design and Application" at TU Hamburg.
 % by Julian Theis and Nicolas Sedlmair, 2021
 
-function []=checkloopshape(L,Llbd,wlbd,Lubd,wubd)
+function []=checkloopshape(L,Llbd,wlbd,Lubd,wubd, varargin)
+
+color = {};
+if nargin == 6
+    color = varargin{1};
+end
 
 % ------ preparations ----------
 
@@ -20,7 +25,7 @@ log_max = 3;
 
 Ts = L.Ts;
 if Ts ~= 0
-    w_sample = pi/Ts; 
+    w_sample = pi/Ts;
     log_max = log10(w_sample);
 end
 
@@ -31,7 +36,7 @@ ymax = 50;
 
 Nw = 1000;
 w = logspace(log_min,log_max,Nw)';
-Lmag = bode(L,w);
+[Lmag, Lphase] = bode(L,w);
 Lmag = Lmag(:);
 LmagdB = 20*log10(Lmag);
 [~,Lidx] = min(abs(LmagdB));
@@ -41,21 +46,25 @@ idx = (w >= wubd);
 idxwubd = find(idx,1,'first');
 
 % ------------------------------
-
-% semilogx(w,LmagdB,'k','LineWidth',0.5);
-semilogx(w,LmagdB,'LineWidth',0.5);
+if isempty(color)
+    semilogx(w,LmagdB,'LineWidth',0.5);
+else
+    semilogx(w,LmagdB, color, 'LineWidth',0.5);
+end
 hold on;
-axis([xmin xmax ymin ymax]);
-semilogx([w(1) w(end)],[0 0],'k');
-lhL = semilogx([wL wL xmin],[ymin 0 0],'k:');
+axis([xmin xmax ymin ymax]); 
+semilogx([w(1) w(end)],[0 0],'k'); 
+% lhL = semilogx([wL wL xmin],[ymin 0 0],'k:');
 % phLclb  = patch([wL/sqrt(10) wL  wL*sqrt(10) wL*sqrt(10) wL wL/sqrt(10)], [10 0 -15 ymin ymin ymin],'g');
 % phLcub  = patch([wL/sqrt(10) wL  wL*sqrt(10) wL*sqrt(10) wL wL/sqrt(10)], [15 0 -10 ymax ymax ymax],'g');
-semilogx([wL/sqrt(10) wL  wL*sqrt(10)],[10 0 -15],'g--');
-semilogx([wL/sqrt(10) wL  wL*sqrt(10)],[15 0 -10],'g--');
+
+% semilogx([wL/sqrt(10) wL  wL*sqrt(10)],[10 0 -15],'g', 'LineWidth',0.1);
+% semilogx([wL/sqrt(10) wL  wL*sqrt(10)],[15 0 -10],'g', 'LineWidth',0.1);
+
 % phLlb = patch([w(1) wlbd wlbd w(1)],[ymin ymin Llbd Llbd],'b');
 % phLub = patch([w(end) wubd wubd w(end)],[ymax ymax Lubd LmagdB(idxwubd)-20],'r');
-semilogx([w(1) wlbd],[Llbd Llbd],'b');
-semilogx([wlbd wlbd],[ymin Llbd],'b');
+semilogx([w(1) wlbd],[Llbd Llbd],'r');
+semilogx([wlbd wlbd],[ymin Llbd],'r');
 semilogx([wubd w(end)],[Lubd -40],'r');
 semilogx([wubd wubd],[Lubd ymax],'r');
 % set(phLlb,'FaceAlpha',0.15,'LineStyle','none');
--- design-support/test/test_step.m
+++ design-support/test/test_step.m
@@ -5,7 +5,7 @@ rho = model_airdata(alt).density;
 
 V = linspace(30, 900, 20);
 P = 0.5 * rho * V.^2;
-steptime = 20;
+steptime = 5;
 T_sample = 0.005; % sampling time of the loop
 
 clear control_scheduler
@@ -18,14 +18,14 @@ for i=1:length(P)
     K = Ks(1:3);
 
     [A, B, C, ~] = model_roll(P(i), CL);
-    sys_plant = c2d(ss(A, B, eye(3), 0), T_sample);
+    sys_plant = c2d(ss(A, B, eye(3), 0), T_sample, 'zoh');
     [phi, gamma] = ssdata(sys_plant);
 
     %%% rolloff filter
     f_rolloff = 100; % [rad/s] rolloff frequency
     lowpass = c2d(tf(f_rolloff, [1, f_rolloff]), T_sample);
 
-    sys_ol = K_pre * K * ss(phi, gamma, eye(3), 0, T_sample);% * lowpass;
+    sys_ol = - K_pre * ss(phi, gamma, K, 0, T_sample);
     sys_cl = K_pre * ss(phi+gamma*K, gamma, eye(3), 0, T_sample);
     
 
@@ -39,33 +39,135 @@ for i=1:length(P)
     end
     % step(sys_cl, steptime);
     % hold on
-
-
 end
 
 %% Figure
-figure(1)
-step(sys_array, steptime)
-hold on
-step(sys_min, 'g', sys_max, 'r', steptime)
-hold off
+load("plots\mumColors.mat")
 
-figure(2)
-bode(sys_array(1,1))
-hold on
-bode(sys_min(1,1), 'g', sys_max(1,1), 'r')
-hold off
-%%
-figure(3)
-for i=1:length(P)   
-    checkloopshape(sys_array_open(1,1,1,i), 20, 0.1, -20, 50)
-    hold on
+set(groot, 'defaultAxesTickLabelInterpreter','latex')
+set(groot, 'defaultLegendInterpreter','latex')
+set(groot, 'DefaultTextInterpreter', 'latex')
+
+
+t = 0:T_sample:steptime;
+u = ones(length(t),1);
+yout = zeros(length(t), 3, length(P));
+for i = 1:length(P)
+    yout(:,:,i) = lsim(sys_array(:,:,1,i), u, t);
 end
-checkloopshape(ss(phi, gamma, [1,0,0], 0, T_sample), 20, 0.1, -20, 50)
-hold off
-% hold on
-% bode(sys_min(1,1), 'g', sys_max(1,1), 'r')
-% hold off
 
-% [margin1, margin2] = margin(sys_array(1,1))
-% [margin] = margin(sys_min(1,1), 'g', sys_max(1,1), 'r')
+f_step = figure(1);
+names = ["$\phi$", "$\omega_x$", "$\delta$"];
+for k = 1:3
+    subplot(3,1,k)
+    for i = 2:length(P)-1
+        plot(t, yout(:,k,i), "Color", col.blue);
+        hold on
+    end
+    plot(t, yout(:,k,1), "Color", col.green, "LineWidth",1);
+    plot(t, yout(:,k,length(P)), "Color", col.red, "LineWidth",1);
+    xlabel("Time [s]")
+    ylabel(names(k))
+    % grid on
+end
+fontsize(f_step, 12, "points")
+
+
+w = logspace(-2,3,1000)';
+for i = 1:length(P)
+    [gain{i}, phase{i}] = bode(sys_array(1,1,1,i),w);
+end
+
+f_bode = figure(2);
+subplot(2,1,1)
+for i = 2:length(P)-1
+    g = gain{i};
+    g = squeeze(g(1,1,:));
+    semilogx(w, 20*log10(g), "Color", col.blue)
+    hold on   
+end
+g = gain{1}; g = squeeze(g(1,1,:));
+semilogx(w, 20*log10(g), "Color", col.green, "LineWidth",1)
+g = gain{end}; g = squeeze(g(1,1,:));
+semilogx(w, 20*log10(g), "Color", col.red, "LineWidth",1)
+xlim([1e-2, pi/T_sample])
+xlabel("Frequency [rad/s]")
+ylabel("Gain [dB]")
+ylim([-100, 0])
+yticks(-100:20:0)
+grid on
+hold off
+subplot(2,1,2)
+for i = 1:length(P)
+    p = phase{i};
+    p = squeeze(p(1,1,:));
+    semilogx(w, p, "Color", col.blue)
+    hold on   
+end
+p = phase{1}; p = squeeze(p(1,1,:));
+semilogx(w, p, "Color", col.green, "LineWidth",1)
+p = phase{end}; p = squeeze(p(1,1,:));
+semilogx(w, p, "Color", col.red, "LineWidth",1)
+xlim([1e-2, pi/T_sample])
+ylim([-360, 0])
+yticks(-360:90:0)
+xlabel("Frequency [rad/s]")
+ylabel("Phase [deg]")
+grid on
+hold off
+fontsize(f_bode, 12, "points")
+
+
+w = logspace(-2,3,1000)';
+for i = 1:length(P)
+    [gain{i}, phase{i}] = bode(sys_array_open(1,1,1,i),w);
+end
+
+f_bode_open = figure(3);
+subplot(2,1,1)
+for i = 2:length(P)-1
+    g = gain{i};
+    g = squeeze(g(1,1,:));
+    semilogx(w, 20*log10(g), "Color", col.blue)
+    hold on   
+end
+g = gain{1}; g = squeeze(g(1,1,:));
+semilogx(w, 20*log10(g), "Color", col.green, "LineWidth",1)
+g = gain{end}; g = squeeze(g(1,1,:));
+semilogx(w, 20*log10(g), "Color", col.red, "LineWidth",1)
+yline(0, "k-")
+xlim([1e-2, pi/T_sample])
+xlabel("Frequency [rad/s]")
+ylabel("Gain [dB]")
+ylim([-60, 60])
+yticks(-60:20:60)
+grid on
+hold off
+
+subplot(2,1,2)
+for i = 1:length(P)
+    p = phase{i};
+    p = squeeze(p(1,1,:));
+    semilogx(w, p, "Color", col.blue)
+    hold on   
+end
+p = phase{1}; p = squeeze(p(1,1,:));
+semilogx(w, p, "Color", col.green, "LineWidth",1)
+p = phase{end}; p = squeeze(p(1,1,:));
+semilogx(w, p, "Color", col.red, "LineWidth",1)
+xlim([1e-2, pi/T_sample])
+ylim([-180, -90])
+yticks(-180:45:-90)
+xlabel("Frequency [rad/s]")
+ylabel("Phase [deg]")
+grid on
+hold off
+fontsize(f_bode_open, 12, "points")
+
+exportgraphics(f_step, 'plots/plot_step.pdf', 'ContentType', 'vector')
+exportgraphics(f_bode, 'plots/plot_bode.pdf', 'ContentType', 'vector')
+exportgraphics(f_bode_open, 'plots/plot_bode_open.pdf', 'ContentType', 'vector')
+
+set(groot, 'defaultAxesTickLabelInterpreter','remove')
+set(groot, 'defaultLegendInterpreter','remove')
+set(groot, 'DefaultTextInterpreter', 'remove')
