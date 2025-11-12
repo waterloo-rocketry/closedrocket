e51fcb7e01f5ecdae7cc12dc56799758cda7ab95
FinnBreu <finn.breuer@tuhh.de>
2025-11-12 17:44:56 +0100

    HIL and other plots

--- monte-carlo/plot/plot_state.m
+++ monte-carlo/plot/plot_state.m
@@ -14,67 +14,75 @@ function [plots] = plot_state(dataset, varargin)
     else
         name = "";
     end
+
+    time = seconds(dataset.Time);
     
     % names = append(["qw","qx","qy","qz"],name);
-    names = ["qw","qx","qy","qz"];
+    names = ["$q_w$","$q_x$","$q_y$","$q_z$"];
     for i = 1:4
-        plot(plots.q, dataset.Time, dataset.q(:,i), 'DisplayName', names(i));
+        plot(plots.q, time, dataset.q(:,i), 'DisplayName', names(i));
         hold(plots.q, 'on')
     end
-    legend(plots.q, 'show')
+    legend(plots.q, 'show', 'Orientation','vertical', Location='eastoutside' )
     title(plots.q, "Quaternion",'FontWeight','Normal')
+    xlabel(plots.q, "Time [s]")
     grid(plots.q, 'on')
 
     % names = append(["wx","wy","wz"],name);
-    names = ["wx","wy","wz"];
+    names = ["$\omega_x$","$\omega_y$","$\omega_z$"];
     for i = 1:3
-        plot(plots.w, dataset.Time, dataset.w(:,i), 'DisplayName', names(i))
+        plot(plots.w, time, dataset.w(:,i), 'DisplayName', names(i))
         hold(plots.w, 'on')
     end
-    legend(plots.w, 'show')
-    title(plots.w, "Rates [rad/s]",'FontWeight','Normal')
+    legend(plots.w, 'show', 'Orientation','vertical', Location='eastoutside' )
+    title(plots.w, "Angular velocity [rad/s]",'FontWeight','Normal')
+    xlabel(plots.w, "Time [s]")
     grid(plots.w, 'on')
 
     % names = append(["vx","vy","vz"],name);
-    names = ["vx","vy","vz"];
+    names = ["$v_x$","$v_y$","$v_z$"];
     for i = 1:3
-        plot(plots.v, dataset.Time, dataset.v(:,i), 'DisplayName', names(i))
+        plot(plots.v, time, dataset.v(:,i), 'DisplayName', names(i))
         hold(plots.v, 'on')
     end
-    legend(plots.v, 'show')
+    legend(plots.v, 'show', 'Orientation','vertical', Location='eastoutside' )
     title(plots.v, "Velocity [m/s]",'FontWeight','Normal')
+    xlabel(plots.v, "Time [s]")
     grid(plots.v, 'on')
 
     % names = append("alt",name);
-    names = "alt";
+    names = "$r_x$";
     for i = 1
-        plot(plots.alt, dataset.Time, dataset.alt(:,i), 'DisplayName', names(i))
+        plot(plots.alt, time, dataset.alt(:,i)/1000, 'DisplayName', names(i))
         hold(plots.alt, 'on')
     end
-    legend(plots.alt, 'show')
+    legend(plots.alt, 'show', 'Orientation','vertical', Location='eastoutside' )
     title(plots.alt, "Altitude [km]",'FontWeight','Normal')
-    % legend(plots.alt, 'show')
-    title(plots.alt, "Altitude [m]",'FontWeight','Normal')
+    % legend(plots.alt, 'show', 'Orientation','vertical', Location='eastoutside' )
+    title(plots.alt, "Altitude [km]",'FontWeight','Normal')
+    xlabel(plots.alt, "Time [s]")
     grid(plots.alt, 'on')
 
-    names = append("CL",name);
-    names = "CL";
+    % names = append("CL",name);
+    names = "$C_{L_\delta}$";
     for i = 1
-        plot(plots.cl, dataset.Time, dataset.cl(:,i), 'DisplayName', names(i))
+        plot(plots.cl, time, dataset.cl(:,i), 'DisplayName', names(i))
         hold(plots.cl, 'on')
     end
-    legend(plots.cl, 'show')
+    legend(plots.cl, 'show', 'Orientation','vertical', Location='eastoutside' )
     title(plots.cl, "Canard Coefficient",'FontWeight','Normal')
+    xlabel(plots.cl, "Time [s]")
     grid(plots.cl, 'on')
 
     % names = append("delta",name);
-    names = "delta";
+    names = "$\delta$";
     for i = 1
-        plot(plots.delta, dataset.Time, rad2deg(dataset.delta(:,i)), 'DisplayName', names(i))
+        plot(plots.delta, time, rad2deg(dataset.delta(:,i)), 'DisplayName', names(i))
         hold(plots.delta, 'on')
     end
-    legend(plots.delta, 'show')
+    legend(plots.delta, 'show', 'Orientation','vertical', Location='eastoutside' )
     title(plots.delta, "Canard Angle [deg]",'FontWeight','Normal')
+    xlabel(plots.delta, "Time [s]")
     grid(plots.delta, 'on')
 
     if  nargin == 3 || nargin == 4
