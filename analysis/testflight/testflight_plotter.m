469c5a41f22334112fd350ae31227b10e35fde2e
FinnBreu <finn.breuer@tuhh.de>
2025-11-21 11:45:33 +0100

    combined plots

--- analysis/testflight/testflight_plotter.m
+++ analysis/testflight/testflight_plotter.m
@@ -13,83 +13,111 @@ set(groot, 'defaultAxesTickLabelInterpreter','latex')
 set(groot, 'defaultLegendInterpreter','latex')
 set(groot, 'DefaultTextInterpreter', 'latex')
 
-f_q = figure(1);
+f_tf = figure(1);
+tiledlayout(3,2,'TileSpacing','Compact');
+
+% f_q = figure(1);
+p_q = nexttile;
 plot(T.timestamp_s, T.ATT_Q0,  'Color', colors(1,:), 'DisplayName', '$q_w$'); hold on;
 plot(T.timestamp_s, T.ATT_Q1,  'Color', colors(2,:), 'DisplayName', '$q_x$');
 plot(T.timestamp_s, T.ATT_Q2,  'Color', colors(3,:), 'DisplayName', '$q_y$');
 plot(T.timestamp_s, T.ATT_Q3,  'Color', colors(4,:), 'DisplayName', '$q_z$');
 xlabel("Time [s]")
-ylabel("Quaternion")
+% ylabel("Quaternion")
+title("Quaternion")
 ylim([-1, 1])
 grid on
 % title("Attitude quaternion") 
-legend('Location','southwest'); hold off;
+% legend('Location','southwest'); 
+legend('Orientation','vertical', Location='eastoutside', IconColumnWidth=12)
+hold off;
 
 
-f_e = figure(2);
+% f_e = figure(2);
+nexttile;
 plot(T.timestamp_s, T.euler_roll, 'Color', colors(2,:), 'DisplayName', '$\phi$'); hold on;
 plot(T.timestamp_s, T.euler_pitch, 'Color', colors(3,:), 'DisplayName', '$\theta$');
 plot(T.timestamp_s, T.euler_yaw, 'Color', colors(4,:), 'DisplayName', '$\psi$');
 xlabel("Time [s]")
-ylabel("Angle [rad]")
+% ylabel("Angle [rad]")
+title("Euler angle [rad]")
 grid on
 % title("Relative Euler angles")
-legend('Location','southwest'); hold off;
+% legend('Location','southwest'); 
+legend('Orientation','vertical', Location='eastoutside', IconColumnWidth=12)
+hold off;
 
-f_w = figure(3);
+% f_w = figure(3);
+nexttile;
 plot(T.timestamp_s, T.RATE_WX,  'Color', colors(2,:), 'DisplayName', '$\omega_x$'); hold on;
 plot(T.timestamp_s, T.RATE_WY,  'Color', colors(3,:), 'DisplayName', '$\omega_y$')
 plot(T.timestamp_s, T.RATE_WZ,  'Color', colors(4,:), 'DisplayName', '$\omega_z$')
 xlabel("Time [s]")
-ylabel("Angular velocity [rad/s]")
+% ylabel("Angular velocity [rad/s]")
+title("Angular velocity [rad/s]")
 grid on
 % title("Angular rates")
-legend('Location','northwest'); hold off;
+% legend('Location','northwest'); 
+legend('Orientation','vertical', Location='eastoutside', IconColumnWidth=12)
+hold off;
 
-f_v = figure(4);
+% f_v = figure(4);
+nexttile;
 plot(T.timestamp_s, T.VEL_VX,  'Color', colors(2,:), 'DisplayName', '$v_x$'); hold on;
 plot(T.timestamp_s, T.VEL_VY,  'Color', colors(3,:), 'DisplayName', '$v_y$');
 plot(T.timestamp_s, T.VEL_VZ,  'Color', colors(4,:), 'DisplayName', '$v_z$');
 xlabel("Time [s]")
-ylabel("Velocity [m/s]")
+% ylabel("Velocity [m/s]")
+title("Velocity [m/s]")
 grid on
 % title("Velocity")
-legend('Location','best'); hold off;
-
-f_a = figure(5);
-plot(T.timestamp_s, T.ALT, 'b', 'DisplayName', '$r_x$')
-xlabel("Time [s]")
-ylabel("Altitude [m]")
-grid on
-% title("Altitude")
-legend('Location','best'); 
+% legend('Location','best'); 
+legend('Orientation','vertical', Location='eastoutside', IconColumnWidth=12)
 hold off;
 
-f_c = figure(6);
+% f_a = figure(5);
+nexttile;
+plot(T.timestamp_s, T.ALT, 'b', 'DisplayName', '$r_x$')
+xlabel("Time [s]")
+% ylabel("Altitude [m]")
+title("Altitude [m]")
+grid on
+% title("Altitude")
+% legend('Location','best');
+legend('Orientation','vertical', Location='eastoutside', IconColumnWidth=12)
+hold off;
+
+% f_c = figure(6);
+nexttile;
 plot(T.timestamp_s, rad2deg(T.CANARD_ANGLE),  'Color', colors(2,:), 'DisplayName', '$\delta$'); hold on;
 plot(T.timestamp_s, T.COEFF_CL,  'Color', colors(4,:), 'DisplayName', '$C_L$')
 xlabel("Time [s]")
-ylabel("Angle [deg], Coefficient")
+% ylabel("Angle [deg], Coefficient")
+title("Angle [deg], Coefficient")
 ylim([-1,5])
 grid on
 % title("Canard")
-legend('Location','best'); hold off;
+% legend('Location','best'); 
+legend('Orientation','vertical', Location='eastoutside', IconColumnWidth=12)
+hold off;
 
 set(groot, 'defaultAxesTickLabelInterpreter','remove')
 set(groot, 'defaultLegendInterpreter','remove')
 set(groot, 'DefaultTextInterpreter', 'remove')
 
-fontsize(f_q, 12, "points")
-fontsize(f_e, 12, "points")
-fontsize(f_w, 12, "points")
-fontsize(f_v, 12, "points")
-fontsize(f_a, 12, "points")
-fontsize(f_c, 12, "points")
+fontsize(f_tf, 12, "points")
+% fontsize(f_q, 12, "points")
+% fontsize(f_e, 12, "points")
+% fontsize(f_w, 12, "points")
+% fontsize(f_v, 12, "points")
+% fontsize(f_a, 12, "points")
+% fontsize(f_c, 12, "points")
 
 %% export
-exportgraphics(f_q, 'analysis/testflight/testflight_q.pdf')
-exportgraphics(f_e, 'analysis/testflight/testflight_euler.pdf')
-exportgraphics(f_w, 'analysis/testflight/testflight_w.pdf')
-exportgraphics(f_v, 'analysis/testflight/testflight_v.pdf')
-exportgraphics(f_a, 'analysis/testflight/testflight_alt.pdf')
-exportgraphics(f_c, 'analysis/testflight/testflight_canard.pdf')
+exportgraphics(f_tf, 'analysis/testflight/testflight.pdf')
+% exportgraphics(f_q, 'analysis/testflight/testflight_q.pdf')
+% exportgraphics(f_e, 'analysis/testflight/testflight_euler.pdf')
+% exportgraphics(f_w, 'analysis/testflight/testflight_w.pdf')
+% exportgraphics(f_v, 'analysis/testflight/testflight_v.pdf')
+% exportgraphics(f_a, 'analysis/testflight/testflight_alt.pdf')
+% exportgraphics(f_c, 'analysis/testflight/testflight_canard.pdf')
--- design-support/test/test_atmosphere.m
+++ design-support/test/test_atmosphere.m
@@ -15,37 +15,45 @@ set(groot, 'defaultAxesTickLabelInterpreter','latex')
 set(groot, 'defaultLegendInterpreter','latex')
 set(groot, 'DefaultTextInterpreter', 'latex')
 
-f_p = figure(1);
+f_atm = figure(1);
+
+tiledlayout(1,3,'TileSpacing','Compact');
+
+plot_p = nexttile;
 plot(p/1000, alt)
-yline([max(alt), 32, 20, 11],'--', {"Stratosphere 2", "Stratosphere 1", "Tropopause", "Troposphere"},'LabelVerticalAlignment','bottom', 'Interpreter','latex')
+yline([max(alt), 32, 20, 11],':', {"Stratosphere 2", "Stratosphere 1", "Tropopause", "Troposphere"},'LabelVerticalAlignment','bottom', 'Interpreter','latex')
 ylim([min(alt), max(alt)])
 ylabel("Altitude $\ell, \: [\mathrm{km}]$")
 xlabel("Pressure $p, \: [\mathrm{kPa}]$")
 ax = gca;
 ax.XGrid = 'on';
-xticks(0:25:100);
+xticks(linspace(0, 100, 5));
+xtickangle(0)
 fontsize(14,"points")
  
-f_t = figure(2);
+plot_t = nexttile;
 plot(t, alt)
-yline([max(alt), 32, 20, 11],'--', {"Stratosphere 2", "Stratosphere 1", "Tropopause", "Troposphere"},'LabelVerticalAlignment','bottom', 'Interpreter','latex')
+yline([max(alt), 32, 20, 11],':', {"Strato. 2", "Stratosphere 1", "Tropopause", "Troposphere"},'LabelVerticalAlignment','bottom', 'Interpreter','latex')
 ylim([min(alt), max(alt)])
 ylabel("Altitude $\ell, \: [\mathrm{km}]$")
 xlabel("Temperature $T, \: [\mathrm{K}]$")
 ax = gca;
 ax.XGrid = 'on';
-xticks(200:25:300);
+xticks(linspace(200, 300, 3));
+xtickangle(0)
 fontsize(14,"points")
 
-f_d = figure(3);
+plot_rho = nexttile;
 plot(rho, alt)
-yline([max(alt), 32, 20, 11],'--', {"Stratosphere 2", "Stratosphere 1", "Tropopause", "Troposphere"},'LabelVerticalAlignment','bottom', 'Interpreter','latex')
+yline([max(alt), 32, 20, 11],':', {"Stratosphere 2", "Stratosphere 1", "Tropopause", "Troposphere"},'LabelVerticalAlignment','bottom', 'Interpreter','latex')
 ylim([min(alt), max(alt)])
 ylabel("Altitude $\ell, \: [\mathrm{km}]$")
 xlabel("Density $\rho, \: [\mathrm{kg}/\mathrm{m}^3]$")
 ax = gca;
 ax.XGrid = 'on';
-xticks(0:0.4:1.5);
+xticks(linspace(0, 1.2, 5));
+xtickangle(0)
+xlim([0, 1.25])
 fontsize(14,"points")
 
 % figure(4)
@@ -57,13 +65,9 @@ fontsize(14,"points")
 
 %%
 
-fontsize(f_p, 12, "points")
-fontsize(f_t, 12, "points")
-fontsize(f_d, 12, "points")
+fontsize(f_atm, 12, "points")
 
-exportgraphics(f_p, 'plots/plot_atmosphere-pressure.pdf')
-exportgraphics(f_t, 'plots/plot_atmosphere-temperature.pdf')
-exportgraphics(f_d, 'plots/plot_atmosphere-density.pdf')
+exportgraphics(f_atm, 'plots/plot_atmosphere.pdf', 'ContentType', 'vector')
 
 set(groot, 'defaultAxesTickLabelInterpreter','remove')
 set(groot, 'defaultLegendInterpreter','remove')
