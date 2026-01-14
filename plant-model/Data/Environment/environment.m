b5af1b8cf31210b145acb568e501297260017d4d
FinnBreu <finn.breuer@tuhh.de>
2026-01-14 22:56:44 +0100

    activated controller after boost, less wind

--- plant-model/Data/Environment/environment.m
+++ plant-model/Data/Environment/environment.m
@@ -2,12 +2,12 @@
 
 %%% Constant wind
 wind_const_direction = deg2rad(360*rand(1));
-wind_const_strength = 12; % m/s
+wind_const_strength = 5; % m/s
 
 %%% discrete gusts
 wind_gust1_start = 5; % s
 wind_gust1_length = [240 230 220]; % m
-wind_gust1_amplitude = 30; % m/s
+wind_gust1_amplitude = 10; % m/s
 wind_gust1_distribution = (2*rand(3,1)-1) .* [0.01; 0.9; 0.5]; % factor of gust on each axis
 
 wind_gust2_start = 15;
