2ba72a6ec6935053897e6d0a5f7dcfc0624a7ad5
FinnBreu <finn.breuer@tuhh.de>
2026-01-20 17:23:04 +0100

    Added old files and adjusted folder structure

---
+++ gnc/navigation/airdata/model_airdata_jacobian.m
@@ -0,0 +1,59 @@
+function [airdata_altitude, airdata] = model_airdata_jacobian(altitude)
+    % computes air data from altitude, according to US standard atmosphere 
+    % air data: static pressure, temperature, density, local speed of sound
+    % calculations found in Stengel 2004, pp. 30
+
+    % parameters
+    air_gamma = 1.4; % adiabatic index
+    air_R = 287.0579; % specific gas constant for air
+    air_atmosphere = [0, 101325, 288.15, 0.0065; % troposphere
+                      11000, 22632.1, 216.65, 0; % tropopause
+                      20000, 5474.9, 216.65, -0.001; % stratosphere
+                      32000, 868.02, 228.65, -0.0028]; % stratosphere 2
+                      % base height, P_base, T_base, lapse rate;
+    earth_r0 = 6356766; % mean earth radius
+    earth_g0 = 9.81; % zero height gravity
+
+    % geopotential altitude
+    altitude_ratio = earth_r0 / (earth_r0 - altitude);
+    altitude = altitude_ratio * altitude;
+    
+    % select atmosphere behaviour from table
+    layer = air_atmosphere(1,:);
+    if altitude > air_atmosphere(2,1)
+        if altitude < air_atmosphere(3,1)
+            layer = air_atmosphere(2,:);
+        elseif altitude < air_atmosphere(4,1)
+            layer = air_atmosphere(3,:);
+        elseif altitude >= air_atmosphere(4,1)
+            layer = air_atmosphere(4,:);
+        end
+    end
+
+    b = layer(1); % base height
+    P_B = layer(2); % base pressure
+    T_B = layer(3); % base temperature
+    k = layer(4); % temperature lapse rate
+    
+    if k == 0
+        pressure = P_B * exp(-earth_g0*(altitude-b)/(air_R*T_B));
+        pressure_altitude = - P_B*earth_g0 / (T_B*air_R) * (altitude_ratio^2) * exp( - earth_g0*(altitude - b) / (T_B*air_R) );
+    else
+        pressure = P_B * (1 - k/T_B*(altitude-b))^(earth_g0/(air_R*k));
+        pressure_altitude = - P_B*earth_g0 / (T_B*air_R) * (altitude_ratio^2) * ( 1 - k/T_B*(altitude - b) )^(earth_g0/(air_R*k) - 1);
+    end
+    temperature = T_B - k * (altitude - b);
+    temperature_altitude = - k * altitude_ratio^2;
+    density = pressure / (air_R*temperature);
+    density_altitude = 1/air_R * (pressure_altitude * temperature - pressure * temperature_altitude) / temperature^2;
+    mach = sqrt(air_gamma*air_R*temperature);
+    mach_altitude = 1/2 * temperature_altitude * sqrt(air_gamma*air_R / temperature);
+
+    % return values
+    airdata.pressure = pressure;
+    airdata_altitude.pressure = pressure_altitude;
+    airdata.density = density;
+    airdata_altitude.density = density_altitude;
+    airdata.mach = mach;
+    airdata_altitude.mach = mach_altitude;
+end
---
+++ gnc/navigation/dynamics/model_dynamics.m
@@ -0,0 +1,92 @@
+function [x_new] = model_dynamics(dt, x, u)
+    % Computes state derivative with predictive model. Use ODE solver to compute next state.
+    
+    %% decomp
+    % decompose state vector: [q(4); w(3); v(3); alt; Cl; delta]
+    q = x(1:4); w = x(5:7); v = x(8:10); alt = x(11); Cl = x(12); delta = x(13);
+
+    % decompose input vector
+    delta_u = u.cmd; a = u.accel;
+
+    %% load parameters
+    persistent param
+    if isempty(param)
+        param = coder.load("model/model_params.mat");
+    end
+    
+    %% compute rotation matrix 
+    %%% attitude transformation, inertial to body frame
+    S = quaternion_rotmatrix(q);
+
+    %% airdata 
+    airdata = model_airdata(alt);
+
+    %% forces and torques
+    torque = aerodynamics(w, v, airdata, Cl, delta, param);
+    
+    %% time updates
+
+    % quaternion update
+    % q_new = quaternion_update(q, w, dt)
+    q_new = quaternion_increment(q, w, dt);
+    % q_err = q_new - quaternion_update(q, w, dt)
+
+    % rate update
+    w_new = w + dt * param.Jinv * (torque - cross(w, param.J*w));
+    
+    % velocity update 
+    %%% acceleration specific force    
+    v_new = v + dt * (a - cross(w,v) + S*param.g);
+
+    % altitude update
+    v_earth = (S')*v;
+    alt_new = alt + dt * v_earth(1);
+
+    % canard coefficients derivative
+    %%% returns Cl to expected value slowly, to force convergence in EKF
+    %%% * sign(v(1)) for easy flying backwards under chute
+    Cl_theory = airfoil(norm(v), airdata.mach, param) * sign(v(1));
+    Cl_new = Cl + dt * (1/param.tau_cl_alpha * (Cl_theory - Cl)); 
+    
+    % actuator dynamics
+    %%% linear 1st order
+    delta_new = delta + dt * (1/param.tau_est * (delta_u - delta));
+    
+    %% concoct state derivative vector
+    x_new = [q_new; w_new; v_new; alt_new; Cl_new; delta_new];
+end
+
+
+%% aerodynamics
+function [torque] = aerodynamics(w, v, airdata, Cl, delta, param)
+    %%% air data
+    p_dyn = airdata.density / 2 * norm(v)^2;
+
+    %%% angle of attack/sideslip
+    sin_alpha = sin(atan2(v(3),v(1)));
+    sin_beta =  - sin(atan2(v(2),v(1)));
+
+    %%% torques
+    torque_canards = Cl *  delta * param.c_canard * p_dyn *[1;0;0];
+    torque_aero = p_dyn * ( param.c_aero * param.Cn_alpha * [0; sin_alpha; sin_beta] ); 
+            %+ param.Cn_omega*[0; w(2); w(3)] ) * param.c_aero; % commented
+            % out because timeline
+    torque = torque_canards + torque_aero;
+end
+
+function [Cl_theory] = airfoil(airspeed, sonic_speed, param)
+    mach_num = airspeed / sonic_speed;
+
+    if mach_num <= 1
+        Cl_theory = param.Cl_alpha;
+    else
+        cone = acos(1 / mach_num);
+        if cone > param.canard_sweep
+            Cl_theory = 4 / sqrt(mach_num^2 - 1);
+        else
+            m = param.canard_sweep_cot/cot(cone);
+            a = m*(0.38+2.26*m-0.86*m^2);
+            Cl_theory = 2*pi^2*param.canard_sweep_cot / (pi + a);
+        end
+    end
+end
