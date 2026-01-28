5d641ba3a9ce92657102020ffcea41942d3fd84f
FinnBreu <finn.breuer@tuhh.de>
2026-01-28 21:56:30 +0100

    control gains playground

---
+++ gnc/control/controller_gains_angle.m
@@ -0,0 +1,14 @@
+function [K, Kr] = controller_gains_angle(L_delta, Q_phi, Q_omega)
+    % computes the optimal control gains for a flight condition 
+    % L_delta: roll acceleration control derivative, Q_phi: weight of angle error, Q_omega: weight of rate error
+  
+    %% feedback gains
+    K = - sign(L_delta) * [ sqrt(Q_phi), sqrt( 2/abs(L_delta)*sqrt(Q_phi) + Q_omega ) ];
+
+    %% feedforward gain
+    % A = [0, 1; 0, 0];
+    % B = [0; L_delta];
+    % C = [1, 0]; % tracking channel
+    % Kr = -1 / ( C * inv(A+B*K) * B );
+    Kr = - K(1);
+end
