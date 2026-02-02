499855ddbe476eef9f9b11b1af4b040c101def88
FinnBreu <finn.breuer@tuhh.de>
2026-02-02 18:09:45 +0100

    why does the CL steady state error depend so strongly on the dynamic pressure and encoder noise?

--- gnc/control/param_estimator_gradient.m
+++ gnc/control/param_estimator_gradient.m
@@ -22,14 +22,15 @@ function [params, K] = param_estimator_gradient(time, w, d, c)
     %%% set up 
     dt = time - t;
     dw = (w - w_old) / dt; % actual roll acceleration  
-    r = [d; 1]  * c; % regressors, delta (cmd or encoder) and constant disturbance
+    r = [d; 0.1] ; % regressors, delta (cmd or encoder) and constant disturbance
+    r = r * c;
 
     %%% measurement prediction
     dw_hat = r' * p;
 
     %%% correction
-    Q = diag([10, 0.01]); % weights
-    Q = Q / (norm(r)^2 + 0.01); % normalized gradient
+    Q = diag([1, 0.1]); % weights
+    Q = Q / (norm(r)^2); % normalized gradient
     K = Q * r; % gain
     p = p + K * (dw - dw_hat); % correct parameters
     
