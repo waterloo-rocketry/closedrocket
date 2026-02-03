cb68a53f1286f40bce8e61411fdcfad9984b4a4e
FinnBreu <finn.breuer@tuhh.de>
2026-02-04 00:13:12 +0100

    tuning and cutoff for low encoder values (if d < 0.01: d = 0)

--- gnc/control/param_estimator_kf.m
+++ gnc/control/param_estimator_kf.m
@@ -1,4 +1,4 @@
-function [params, K] = param_estimator_kf(time, w, d, c, wdot)
+function [params, K] = param_estimator_kf(time, w, d, c)
 
     % wrote out entire KF math by hand to learn, will consolidate onto one
     % line as we move forward.
@@ -9,15 +9,15 @@ function [params, K] = param_estimator_kf(time, w, d, c, wdot)
     % Cl = canard lift coeff
     % C0 = rocket induced angular acceleration / (rho * area * arm)
 
-    persistent t p_k_minus1 P_k_minus1 w_old d_old
+    persistent t p_k_minus1 P_k_minus1 w_old d_old dw_old
     if isempty(t)
         t = -0.01;
     end
     if isempty(p_k_minus1)
-        p_k_minus1 = [2; 0];
+        p_k_minus1 = [1; 0];
     end
     if isempty(P_k_minus1)
-        P_k_minus1 = diag([1, 1]);
+        P_k_minus1 = diag([0.1, 0.01]);
     end
     if isempty(w_old)
         w_old = w;
@@ -25,18 +25,22 @@ function [params, K] = param_estimator_kf(time, w, d, c, wdot)
     if isempty(d_old)
         d_old = d;
     end
+     if isempty(dw_old)
+        dw_old = 0;
+    end
     
-    % KF constants
+    % KF constant
     dt = time - t;
-    y_k = (w - w_old)/dt; % measurement
+    dw = (w - w_old)/dt;
+    y_k = 0.5*dw + 0.5*dw_old; % measurement
     phi_k = c * [(d + d_old) / 2; 1]; % time aligned with y_k
     F = eye(2); 
-    Q = diag([6e-6 1e-7]); % used for forgetting factor
+    Q = diag([1e-4 1e-6]); % used for forgetting factor
     R = 1;   % scalar (rad/s^2)^2
 
     % prediction
     p_k_minus = F * p_k_minus1;
-    P_k_minus = F * P_k_minus1 * F' + Q;
+    P_k_minus = F * P_k_minus1 * F';% + Q;
     y_k_hat = phi_k' * p_k_minus;
     
     % prediction error
@@ -46,7 +50,7 @@ function [params, K] = param_estimator_kf(time, w, d, c, wdot)
     % kalman gain
     K_k = P_k_minus * phi_k / S_k; % bless matlab i can divide matrices
     p_k_hat = p_k_minus + K_k * nu_k;
-    P_k = (eye(2) - K_k * phi_k') * P_k_minus;
+    P_k = (eye(2) - K_k * phi_k') * P_k_minus + Q;
     
     % update for next cycle
     t = time;
@@ -54,6 +58,7 @@ function [params, K] = param_estimator_kf(time, w, d, c, wdot)
     P_k_minus1 = P_k;
     w_old = w;
     d_old = d;
+    dw_old = dw;
 
     params = p_k_hat;
     K = K_k;
