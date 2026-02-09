792a1959f14208d068948ca8695e103ddea12a40
FinnBreu <finn.breuer@tuhh.de>
2026-02-09 21:52:22 +0100

    moved EKF functions to their own folder

--- gnc/navigation/ekf_algorithm.m
+++ gnc/navigation/ekf_algorithm.m
@@ -8,7 +8,7 @@ function [x, P] = ekf_algorithm(x, P, b, t, T, IMU_1, IMU_2, sensor_select)
 
     %% Prediction step
     %%% Q is a square 13 matrix, tuning for prediction E(noise)
-    %%% x = [   q(4),           w(3),         v(3),    alt(1), Cl(1), delta(1)]
+    %%% x = [   q(4),           w(3),         v(3),    alt(1)]
     Q = diag([[1,1,1,1]*1e-7, [10, 10, 10], [1,1,1]*1e-3, 1]) * 1e-3;
     
     u = zeros(3, 2);
@@ -20,12 +20,20 @@ function [x, P] = ekf_algorithm(x, P, b, t, T, IMU_1, IMU_2, sensor_select)
     %% Correction step(s), sequential for each IMU
     %%% R is a square matrix (size depending on amount of sensors), tuning for measurement E(noise)
      
+    % IMU gyro correction
+    if sensor_select(1) == 1 % only correct with alive IMUs
+        %%% y = [   W(3) ]
+        R = diag([[1, 1, 1])*1e-6;
+        [xhat, Phat] = ekf_correct(@meas_gyro, @meas_gyro_jacobian, x, P, IMU_1(4:6), b.bias_1, R);
+        x = xhat; P = Phat;
+    end
+
     % IMU 1 (MTi630)
     if sensor_select(1) == 1 % only correct with alive IMUs
         %%% y = [   W(3),          Mag(3),      P(1)]
         R = diag([[1, 1, 1]*1e-6, [1, 1, 1]*0.01, 1]);
 
-        [xhat, Phat] = ekf_correct(@model_meas_imu, @model_meas_imu_jacobian, x, P, IMU_1(4:end), b.bias_1, R);
+        [xhat, Phat] = ekf_correct(@model_meas_imu, @model_meas_imu_jacobian, x, P, IMU_1(4:6), b.bias_1, R);
         x = xhat; P = Phat;
     end
 
