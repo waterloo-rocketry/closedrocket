202d81dc7ccdf84b20506be45eca677106f4d85a
FinnBreu <finn.breuer@tuhh.de>
2026-02-10 12:26:07 +0100

    Improved file/functions structure to be more clear. Renamed functions to adhere to this years notation. Added files so each function has its own file.

---
+++ gnc/navigation/dynamics/dynamics_aero_jacobian.m
@@ -0,0 +1,14 @@
+function [torque_v] = dynamics_aero_jacobian(v, alt, param)
+    % aerodynamics
+
+    %%% air data 
+    airdata = model_airdata(alt, v);
+
+    %torque_vx = Cl * delta * param.c_canard * [v(1), v(2), v(3); 0, 0, 0; 0, 0, 0];
+    torque_vyz = 0.5 * param.c_aero * param.Cn_alpha * [0, 0, 0;
+                                                        v(3), 0, v(1);
+                                                        -v(2), -v(1), 0];
+    %torque_v =  airdata.density * (torque_vx + torque_vyz);
+    torque_v =  airdata.density * (torque_vyz);
+
+end
