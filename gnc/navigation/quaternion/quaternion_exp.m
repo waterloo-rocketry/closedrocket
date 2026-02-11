1baafffcb0221154adb4629b30a90c4bbce9eeaf
FinnBreu <finn.breuer@tuhh.de>
2026-02-11 19:40:04 +0100

    some work on quaternion jacobians

--- gnc/navigation/quaternion/quaternion_exp.m
+++
@@ -1,9 +0,0 @@
-function [exp_q] = quaternion_exp(v)
-    % quaternion exponential of vector
-
-    %%% rotation parameters
-    phi = norm(v);
-
-    %%% inverse quaternion 
-    exp_q = [cos(phi); sin(v)];
-  end
