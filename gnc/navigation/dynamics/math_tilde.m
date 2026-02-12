2d3882e03382b7b93e45ee381f29251bf9cfe461
FinnBreu <finn.breuer@tuhh.de>
2026-02-12 18:30:52 +0100

    Researched exact time integration with matrix exponentials

--- gnc/navigation/dynamics/math_tilde.m
+++ gnc/navigation/dynamics/math_tilde.m
@@ -1,4 +1,4 @@
-function [skewed] = tilde(vector)
+function [skewed] = math_tilde(vector)
     % skew symmetric matrix / cross-product jacobian
     skewed = [0, -vector(3), vector(2);
               vector(3), 0, -vector(1);
