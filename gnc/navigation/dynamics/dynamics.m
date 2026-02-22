6b01c1c3b31e3c653f5ea4eddc8cca7ed6257f73
FinnBreu <finn.breuer@tuhh.de>
2026-02-22 17:29:04 +0100

    clarified some comments

--- gnc/navigation/dynamics/dynamics.m
+++ gnc/navigation/dynamics/dynamics.m
@@ -11,34 +11,27 @@ function [x_new] = dynamics(dt, x, a)
         param = coder.load("model_params.mat");
     end
     
-    %% compute rotation matrix 
-    %%% attitude transformation, inertial to body frame
-    S = quaternion_rotmatrix(q);
-
-    %% forces and torques
-    torque = dynamics_aero(v, alt, param);
-    
     %% time updates
-
     % quaternion update
     q_new = quaternion_update(q, w, dt);
 
     % rate update
     w_exp_tilde = math_exp_tilde(w, dt);
+    torque = dynamics_aero(v, alt, param);
     w_new = param.Jinv * (w_exp_tilde*param.J*w) + dt * param.Jinv * torque;
     %w_new = w + dt * param.Jinv * (torque - cross(w, param.J*w)); % old version
-    %%% for Jx < Jy = Jz : u = (Jy-Jx)/Jy * wx, and 
+    %%% possibly more accurate: for Jx < Jy = Jz : u = (Jy-Jx)/Jy * wx, and 
     %%% wx_new = wx, [wy, wz]_new = Sx(u*dt)*[wy, wz] with Sx = [c(u), s(u); -s(u), c(u)]
     
     % velocity update 
+    S = quaternion_rotmatrix(q); % inertial to body frame
     v_new = w_exp_tilde*v + dt * a + dt * S*param.g; 
     % v_new = v + dt * (a - cross(w,v) + S*param.g); % old version
         
     % altitude update
     v_earth = (S')*v;
     alt_new = alt + dt * v_earth(1);
-
     
-    %% concoct state derivative vector
+    %% concoct state update vector
     x_new = [q_new; w_new; v_new; alt_new];
 end
