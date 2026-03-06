091b10872567d2e9a90bc3bf1aca6ea89263bd30
FinnBreu <finn.breuer@tuhh.de>
2026-03-06 22:19:42 +0100

    Updated sensor data for Polaris. Split environment data into two scripts. Separate sampling rates for sensors/nav/control

--- plant-model/Data/Environment/wind_historic.m
+++ plant-model/Data/Environment/wind_historic.m
@@ -1,26 +1,4 @@
-%% Winds
-
-%%% Constant wind
-wind_const_direction = deg2rad(360*rand(1));
-wind_const_strength = 5; % m/s
-
-%%% discrete gusts
-wind_gust1_start = 5; % s
-wind_gust1_length = [240 230 220]; % m
-wind_gust1_amplitude = 10; % m/s
-wind_gust1_distribution = (2*rand(3,1)-1) .* [0.01; 0.9; 0.5]; % factor of gust on each axis
-
-wind_gust2_start = 15;
-wind_gust2_length = [400 300 500]; % m
-wind_gust2_amplitude = 30; % m/s
-wind_gust2_distribution = (2*rand(3,1)-1) .* [-0.01; -0.5; -0.8]; % factor of gust on each axis
-
-wind_gust3_start = 80;
-wind_gust3_length = [400 300 500]; % m
-wind_gust3_amplitude = 10; % m/s
-wind_gust3_distribution = (2*rand(3,1)-1) .* [0.01; 1; 1]; % factor of gust on each axis
-
-%%% windspeed wrt height from csv [new! and shinier!!]
+%% windspeed wrt height from csv
 wind_data = readtable("plant-model/Data/environment/sim_parameters_historical_aug2023+aug2024_no-clouds_2025-06-03.csv", ...
     ReadVariableNames=true, VariableNamingRule="preserve");
 
@@ -29,7 +7,7 @@ row_idx = randi(height(wind_data));
 % row_idx = 1;
 row = wind_data(row_idx, :);
 wind_time = row.date;
-fprintf('Selected timestamp: %s\n', wind_time);
+fprintf('Selected historic wind date: %s\n', wind_time);
 
 cols = wind_data.Properties.VariableNames;
 
--- plant-model/configure_plant_model.m
+++ plant-model/configure_plant_model.m
@@ -9,13 +9,14 @@ chute_enable = [1, 0]; % no recovery is = 0
 time_idle = 10; % wait time on the rail before launch
 
 %%% environment
-run('plant-model/Data/Environment/environment.m')
+run('plant-model/Data/Environment/wind_discrete.m')
+run('plant-model/Data/Environment/wind_historic.m')
 % enable wind disturbances
 wind_dist_enable = 1; % no disturbances is = 0
-constant_wind_enable = 0;   % Toggle for type; old wind model is = 1
+constant_wind_enable = 0;   % Toggle for type; historic layered wind is 0, old wind model is = 1
 
 %%% sensors
-run('plant-model/Data/Sensors/sensors_processing.m')
+run('plant-model/Data/Sensors/sensors_polaris.m')
 
 %% data pre-processing
 run('plant-model/scripts/data_preparation.m')
