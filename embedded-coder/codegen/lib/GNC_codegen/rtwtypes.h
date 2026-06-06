d0065e110e1d7a44360feb71f6ac0bdde60dc879
Tristan Yan-Klassen <emptrex@gmail.com>
2026-06-06 15:21:30 -0400

    codegenned for arm cortex m7

--- embedded-coder/codegen/lib/GNC_codegen/rtwtypes.h
+++ embedded-coder/codegen/lib/GNC_codegen/rtwtypes.h
@@ -11,12 +11,12 @@ extern "C" {
 
 /*=======================================================================*
  * Target hardware information
- *   Device type: Generic->32-bit Embedded Processor
+ *   Device type: ARM Compatible->ARM Cortex-M
  *   Number of bits:     char:   8    short:   16    int:  32
  *                       long:  32
  *                       native word size:  32
- *   Byte ordering: Unspecified
- *   Signed integer division rounds to: Undefined
+ *   Byte ordering: LittleEndian
+ *   Signed integer division rounds to: Zero
  *   Shift right on a signed integer as arithmetic shift: on
  *=======================================================================*/
 
